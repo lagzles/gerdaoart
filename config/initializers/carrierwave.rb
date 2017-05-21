605d7bb71d1894d0b1a5698281fc2edcd7a551bb
lagzles <leandro.arns@gmail.com>
2017-05-21 11:34:39 -0300

    barra de navegação inicial

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -41,4 +41,4 @@
   config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
-end
+# end
