b8f44d8bb789ba051141c8e40191dfd713e2b2a4
lagzles <leandro.arns@gmail.com>
2017-05-29 23:26:08 -0300

    background image, ajuste de forms e grids

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,6 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
+  before_action :set_drawing, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @images = Image.all
@@ -21,11 +22,13 @@ class ImagesController < ApplicationController
     #   category = Category.find_or_create_by(name: "#{params[:category]}")
     #   @carousel.category_id = category.id
     # end
-
+    @image.drawing_id = params[:drawing_id]
+    puts "#{params[:drawing_id]}"
+    puts "#{@image.drawing_id}"
     if @image.valid?
       @image.save
       # redirect_to carousels_path, notice: 'Carousel was successfully created.'
-      redirect_to drawing_path @image.drawing, notice: 'Carousel was successfully created.'
+      redirect_to drawing_path @image.drawing, notice: 'Image was successfully created.'
     else
        render :new
     end
@@ -33,22 +36,26 @@ class ImagesController < ApplicationController
 
   def update
     if @image.update(image_params)
-      redirect_to drawing_path @image.drawing, notice: 'Carousel was successfully updated.'
+      redirect_to drawing_path @image.drawing, notice: 'Image was successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
+    drawing = @image.drawing
     @image.destroy
-    redirect_to drawings_url, notice: 'Carousel was successfully destroyed.'
-    
+    redirect_to drawing_url(drawing), notice: 'Image was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_image
       @image = Image.find(params[:id])
+    end
+
+    def set_drawing
+      @drawing = Drawing.find(params[:drawing_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
