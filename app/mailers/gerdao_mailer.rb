ccdcab59c50256f62b8d3f3ee9568c94929f0b3f
lagzles <leandro.arns@gmail.com>
2017-07-01 10:17:44 -0300

    adicionado mailer 'gerdao_mailer'

---
+++ app/mailers/gerdao_mailer.rb
@@ -0,0 +1,5 @@
+class GerdaoMailer < ApplicationMailer
+
+  default from: "Email@email.com"
+
+end
---
+++ test/mailers/gerdao_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GerdaoMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
