4dacc0a296a4b4cf742a923440c239579b62de89
lagzles <leandro.arns@gmail.com>
2017-07-22 11:45:03 -0300

    ajuste de bootstrap, e css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 // Bootstrap
-//= require bootstrap-sprockets
-//= require bootstrap
+// require bootstrap-sprockets
+// require bootstrap
 // require turbolinks
 // require_tree .
